9e28d09d21da011f6874af5f3e122f8ac0e1ed70
Pierre Rolland <roll.pierre@gmail.com>
2022-05-05 23:14:05 +0200

    Added json object gatherable

---
+++ spec/RollAndRock/Reket/Type/Json/JsonObjectSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\RollAndRock\Reket\Type\Json;
+
+use PhpSpec\ObjectBehavior;
+use RollAndRock\Reket\Type\ExternalField;
+use RollAndRock\Reket\Type\Field;
+use RollAndRock\Reket\Type\Gatherable;
+use RollAndRock\Reket\Type\GatherableExpression;
+use RollAndRock\Reket\Type\Json\JsonObject;
+
+class JsonObjectSpec extends ObjectBehavior
+{
+    function let(Field $field, ExternalField $externalField, GatherableExpression $expression, Gatherable $gatherable)
+    {
+        $this->beConstructedWith([
+            $field,
+            $externalField,
+            $expression,
+            $gatherable
+        ]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(JsonObject::class);
+    }
+
+    function its_to_sql_returns_json_object_sql_string(
+        Field $field,
+        ExternalField $externalField,
+        GatherableExpression $expression,
+        Gatherable $gatherable,
+        Field $baseField
+    ) {
+        $field->getAlias()->willReturn(null);
+        $field->getName()->willReturn('field');
+        $field->toSQL()->willReturn('source1.field');
+        $baseField->getName()->willReturn('base_field');
+        $externalField->getAlias()->willReturn(null);
+        $externalField->getBaseField()->willReturn($baseField);
+        $externalField->toSQL()->willReturn('source2.external_field');
+        $expression->getAlias()->willReturn('expression');
+        $expression->toSQL()->willReturn('SELECT(...)');
+        $gatherable->getAlias()->willReturn(null);
+        $gatherable->toSQL()->willReturn('COUNT');
+
+        $this->toSQL()->shouldReturn("JSON_BUILD_OBJECT('field', source1.field, 'base_field', source2.external_field, 'expression', SELECT(...), 'COUNT', COUNT)");
+    }
+    function its_to_sql_returns_aliased_sql_string()
+    {
+        $this->beConstructedWith([], 'the_alias');
+
+        $this->toSQL()->shouldReturn('JSON_BUILD_OBJECT() AS the_alias');
+    }
+
+}
---
+++ src/Type/Json/JsonObject.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RollAndRock\Reket\Type\Json;
+
+use RollAndRock\Reket\Type\ExternalField;
+use RollAndRock\Reket\Type\Field;
+use RollAndRock\Reket\Type\Gatherable;
+
+class JsonObject implements Gatherable
+{
+    private ?string $alias;
+
+    /**
+     * @var Gatherable[]
+     */
+    private array $gatherables;
+
+    public function __construct(array $gatherables, ?string $alias = null)
+    {
+        $this->gatherables = $gatherables;
+        $this->alias = $alias;
+    }
+
+    public function getAlias(): ?string
+    {
+        return $this->alias;
+    }
+
+    public function toSQL(): string
+    {
+        return sprintf(
+            'JSON_BUILD_OBJECT(%s)%s',
+            implode(
+                ', ',
+                array_map(fn (Gatherable $gatherable) => sprintf("'%s', %s", $this->getKey($gatherable), $gatherable->toSQL()), $this->gatherables)
+            ),
+            null !== $this->getAlias() ? sprintf(' AS %s', $this->getAlias()) : ''
+        );
+    }
+
+    private function getKey(Gatherable $gatherable): string
+    {
+        if ($gatherable->getAlias()) {
+            return $gatherable->getAlias();
+        }
+
+        if ($gatherable instanceof Field) {
+            return $gatherable->getName();
+        }
+
+        if ($gatherable instanceof ExternalField) {
+            return $gatherable->getBaseField()->getName();
+        }
+
+        return $gatherable->toSQL();
+    }
+}
